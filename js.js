f0073f2675dc6fc1ddfaf6a3aa85682776a70000
Victor Vague <48601663+SomebodyLikeEveryBody@users.noreply.github.com>
2020-01-12 18:11:48 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,29 @@
+function translate(inText, dictReplace) {
+	let outText = inText;
+
+	for (const key in dictReplace) {
+		outText = outText.replace(new RegExp(key, 'g'), dictReplace[key]);
+	}
+
+	return (outText)
+}
+
+$(function () {
+
+	let inEl =  $('textarea#in_text');
+	let outEl =  $('textarea#out_text');
+
+	inEl.focus();
+	$('button#validate').click(() => {
+		outEl.val(translate(inEl.val(), g_dictReplace));
+		outEl.select();
+	});
+
+	$('button#focus_looper').focus(() => {
+		inEl.focus();
+	});
+
+	inEl.focus(() => {
+		inEl.select();
+	});
+});
