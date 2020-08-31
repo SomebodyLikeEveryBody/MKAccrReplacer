ce12ae6c1c71a91e825b124ac38cfdbef796262a
r0b3rt[0] <nullepart@rien.com>
2020-08-31 23:17:03 +0200

    okay for helper

--- js.js
+++ js.js
@@ -9,6 +9,7 @@ function translate(pInText, pDictReplace) {
 }
 
 function addAZPatternToDict(pDict) {
+
 	for (const key in pDict) {
 		if (/^[A-Z0-9]+$/.test(key) === true) {
 			pDict[key] = '$1' + pDict[key] + '$2';
@@ -18,12 +19,36 @@ function addAZPatternToDict(pDict) {
 	}
 }
 
+function getLastWordFromInText() {
+	let textContent = $('textarea#in_text').val().replaceAll('\n', ' ');
+	let lastWord = textContent.split(' ').pop();
+
+	return (lastWord);
+}
+
+function getAcronymsFromDict(pDict) {
+	let retAcronyms = [];
+
+	for (const key in pDict) {
+		retAcronyms.push(key);
+	}
+
+	return (retAcronyms);
+}
+
+function getKeywordsMatchingWith(pWord, pWords) {
+	return pWords.filter((word) => (word.includes(pWord)));
+}
+
 $(function () {
 
 	let inEl = $('textarea#in_text');
 	let outEl = $('textarea#out_text');
+	let acronyms = getAcronymsFromDict(g_dictReplace);
 
 	addAZPatternToDict(g_dictReplace);
+	getKeywordsMatchingWith('RV', acronyms);
+
 	inEl.focus();
 	$('button#validate').click(() => {
 		outEl.val(translate(inEl.val(), g_dictReplace));
@@ -32,6 +57,17 @@ $(function () {
 
 	$('button#focus_looper').focus(() => {
 		inEl.focus();
+	});
+
+	$('textarea#in_text').keyup(() => {
+		let lastInputWord = getLastWordFromInText();
+
+		if (lastInputWord !== '') {
+			$('textarea#helper').val(getKeywordsMatchingWith(lastInputWord, acronyms).join('\n'));
+		} else {
+			$('textarea#helper').val(acronyms.join('\n'));
+		}
+		
 	});
 
 	inEl.focus(() => {
